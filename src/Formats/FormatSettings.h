50026c78f0da858b2c0c1160859df87e6a22e7bc
Christoph Wurm <wurm.ch@gmail.com>
2023-01-12 11:48:32 +0000

    Revert "Manual backport of #37327 to 22.3"

--- src/Formats/FormatSettings.h
+++ src/Formats/FormatSettings.h
@@ -78,7 +78,6 @@ struct FormatSettings
         bool low_cardinality_as_dictionary = false;
         bool import_nested = false;
         bool allow_missing_columns = false;
-        bool output_string_as_string = false;
     } arrow;
 
     struct
@@ -141,7 +140,6 @@ struct FormatSettings
         UInt64 row_group_size = 1000000;
         bool import_nested = false;
         bool allow_missing_columns = false;
-        bool output_string_as_string = false;
     } parquet;
 
     struct Pretty
@@ -222,7 +220,6 @@ struct FormatSettings
         bool import_nested = false;
         bool allow_missing_columns = false;
         int64_t row_batch_size = 100'000;
-        bool output_string_as_string = false;
     } orc;
 
     /// For capnProto format we should determine how to
